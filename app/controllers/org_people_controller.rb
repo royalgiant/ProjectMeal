3f14652320b3d5855fa287840ef82b88ef04cca4
Donald Lee <donaldlee50@gmail.com>
2015-10-21 23:10:54 -0700

    Episode 19 Edit function and Edit Form

--- app/controllers/org_people_controller.rb
+++ app/controllers/org_people_controller.rb
@@ -1,3 +1,8 @@
 class OrgPeopleController < ApplicationController
-	
+	def edit
+		@person = OrgPerson.find(params[:id])
+		@contactInfo = OrgContact.find_or_initialize_by(org_person_id: params[:id])
+		@contactInfo[:email] = current_org_person.email
+		@person.org_contacts.build(@contactInfo.attributes)
+	end
 end
