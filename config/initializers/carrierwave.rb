cc8857a2c3c5a99da9c9775450285bc612876857
Donald Lee <donaldlee50@gmail.com>
2015-11-07 11:32:00 -0800

    Ruby on Rails Tutorial 31 - Amazon S3 Image Uploading Finale

---
+++ config/initializers/carrierwave.rb
@@ -0,0 +1,32 @@
+CarrierWave.configure do |config|
+  config.fog_credentials = {
+    provider:               'AWS',
+    aws_access_key_id:      ENV['AWS_ACCESS_KEY_ID'],
+    aws_secret_access_key:  ENV['AWS_SECRET_ACCESS_KEY'],
+    region:                 'us-west-2'
+  }
+
+  # For testing, upload files to local `tmp` folder.
+  if Rails.env.test? || Rails.env.cucumber?
+    config.storage           = :file
+    config.enable_processing = false
+    config.root              = "#{Rails.root}/tmp"
+  else
+    config.storage = :fog
+  end
+
+  config.cache_dir        = "#{Rails.root}/tmp/uploads" # To let CarrierWave work on Heroku
+  config.fog_directory    = ENV['S3_BUCKET']
+end
+
+module CarrierWave
+  module MiniMagick
+    def quality(percentage)
+      manipulate! do |img|
+        img.quality(percentage.to_s)
+        img = yield(img) if block_given?
+        img
+      end
+    end
+  end
+end
