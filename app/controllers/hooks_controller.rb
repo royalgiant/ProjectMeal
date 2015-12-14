a7bea077d1cfac519edc8dfde8e2cfdef7cd4cd8
Donald Lee <donaldlee50@gmail.com>
2015-12-13 23:40:18 -0800

    Ruby on Rails Tutorial 90 - Stripe Managed

---
+++ app/controllers/hooks_controller.rb
@@ -0,0 +1,87 @@
+class HooksController < ApplicationController
+	# Webhooks can't possibly know our CSRF token.
+  # So disable that feature entirely for this controller.
+  skip_before_action :verify_authenticity_token
+
+  def stripe
+    # If the request has a 'user_id' key, then this is a webhook
+    # event sent regarding a connected user, and not to a webhook
+    # handler setup on the application owner's account.
+    # So, use the user_id to look up a connected user on our end.
+    user = params[:user_id] && OrgPerson.find_by( stripe_user_id: params[:user_id] )
+
+    # If we didn't find a user, we'll have nil instead
+    # so build our arguments to the Event API taking that into account.
+    # We'll end up with one of:
+    #   args = [ 'EVENT_ID' ]
+    #   args = [ 'EVENT_ID', 'ACCESS_TOKEN' ]
+    args = [ params[:id], user.try(:stripe_secret_key) ].compact
+
+    # Retrieve the event from Stripe so that we can be
+    # sure it wasn't spoofed/faked by someone being mean.
+    begin
+      event = Stripe::Event.retrieve( *args )
+    rescue Stripe::InvalidRequestError
+      # The event doesn't exist for some reason... this might
+      # happen if you've got other apps maybe?
+      render nothing: true, status: 200
+      return
+    rescue Stripe::AuthenticationError
+      # If we get an authentication error, and the event belongs to
+      # a user, that means the account deauthorized
+      # our application. We can't look up and verify the event
+      # because the event belongs to the connected account, and we're
+      # no longer authorized to access their account!
+      if user && user.connected?
+        connector = StripeConnect.new( user )
+        connector.deauthorized
+      end
+
+      render nothing: true, status: 200
+      return
+    end
+
+    # Here we're actually done, but if you wanted to handle
+    # other events (charges or invoice payment failures, etc)
+    # then this is how you would do it.
+    case event.try(:type)
+
+    when 'account.application.deauthorized'
+      # This is what the account.application.deauthorized
+      # handler will hopefully look like some day, where
+      # the event is still accessible somehow and we verified
+      # it came from Stripe.
+      if user && user.connected?
+        user.manager.deauthorized
+      end
+
+    when 'account.updated'
+      # This webhook is used for standalone and managed
+      # accounts. It will notify you about new information
+      # required for the account to remain in good standing.
+      if user && user.connected?
+        # we don't actually need to pass the event here
+        # we'll request the account details directly inside
+        # the manager
+        user.manager.update_account!
+      end
+
+    # These others simply log the event because there's
+    # not much to do with them for this example app.
+    # You might do more useful things like sending receipt emails, etc.
+    # Of course you can handle as many event types as you need:
+    #   https://stripe.com/docs/api#event_types
+    when 'charge.succeeded'
+      Rails.logger.info "**** STRIPE EVENT **** #{event.type} **** #{event.id}"
+    when 'invoice.payment_succeeded'
+      Rails.logger.info "**** STRIPE EVENT **** #{event.type} **** #{event.id}"
+    when 'invoice.payment_failed'
+      Rails.logger.info "**** STRIPE EVENT **** #{event.type} **** #{event.id}"
+
+    end
+
+    # We just need to respond in the affirmative.
+    # No body is necessary.
+    render nothing: true, status: 200
+  end
+end
--- app/models/org_person.rb
+++ app/models/org_person.rb
@@ -11,4 +11,27 @@ class OrgPerson < ActiveRecord::Base
 	serialize :stripe_account_status, JSON
 	acts_as_voter
 	accepts_nested_attributes_for :org_contacts
+
+		# General 'has a Stripe account' check
+		def connected?; !stripe_user_id.nil?; end
+
+		# Stripe account type checks
+		def managed?; stripe_account_type == 'managed'; end
+		# def standalone?; stripe_account_type == 'standalone'; end
+  		# def oauth?; stripe_account_type == 'oauth'; end
+
+  		def manager
+  			case stripe_account_type
+    		when 'managed' then StripeManaged.new(self)
+    		# when 'standalone' then StripeStandalone.new(self)
+    		# when 'oauth' then StripeOauth.new(self)
+    		end
+  		end
+
+  		def can_accept_charges
+  			# return true if oauth?
+    		return true if managed? && stripe_account_status['charges_enabled']
+    		# return true if standalone? && stripe_account_status['charges_enabled']
+    		return false
+  		end
 end
