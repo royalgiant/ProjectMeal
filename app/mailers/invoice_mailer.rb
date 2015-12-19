d9d272c9caec1b8b3c820f61797cd753fbc8eb56
Donald Lee <donaldlee50@gmail.com>
2015-12-19 15:33:52 -0800

    Ruby on Rails Tutorial 101 - InvoiceMailer

---
+++ app/mailers/invoice_mailer.rb
@@ -0,0 +1,23 @@
+class InvoiceMailer < ApplicationMailer
+	def invoice_details(details)
+		@order = details[:order]
+		@company = details[:company]
+		@shipAddress = details[:shipAddress]
+	  	@purchase_items = details[:purchase_items]
+	    @notification_params_name = details[:notification_params]
+	    @total_tax = details[:total_tax]
+  		@currency = details[:currency]
+	  	@contact = details[:billed_contact]
+	  	@pm_fee = details[:pm_fee]
+
+	  	mail(to: details[:billed_contact].email, subject: "ProjectMeal: Your Invoice for Purchase Order ##{detail[:order].id}") do |format|
+	  		format.html
+	  		format.text
+	  		format.pdf do 
+	  			attachments["invoice-#{details[:order].id}.pdf"] = WickedPdf.new.pdf_from_string(
+		          render_to_string(pdf: "invoice-#{@order.id}.pdf", template: "trx_orders/purchase_order.pdf.erb")
+		        )
+		    end
+	  	end	
+	end
+end
