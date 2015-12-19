d244798db269eedc09feaa190fb976dfea696134
Donald Lee <donaldlee50@gmail.com>
2015-12-18 23:34:05 -0800

    Ruby on Rails Tutorial 92 - Stripe Connected Demo

--- app/assets/javascripts/org_people.js
+++ app/assets/javascripts/org_people.js
@@ -48,7 +48,12 @@ $(document).ready(function() {
     $('.org_people.edit').ready(function() {
         return $('li.sidebar_overview').addClass('active');
     });
-    
+    $('.org_people.stripe_settings').ready(function() {
+        $('li.sidebar_stripe_settings').addClass('active');
+        // Stripe Connect code
+	    setupManaged() //pre-connection
+	    setupFieldsNeeded(); // Connected user, but we need info
+    });
     regions = $('#org_person_org_contacts_attributes_0_typ_regions_id').html();
     $('#org_person_org_contacts_attributes_0_typ_countries_id').change(function() {
 	    var country, escaped_country, options;
@@ -65,4 +70,62 @@ $(document).ready(function() {
 	});
 });
 
+setupManaged = function() {
+	var container, countrySelect, createButton, form, tosEl;
+  	container = $('#stripe-managed');
+	if (container.length === 0) {
+	    return;
+	}
+  	tosEl = container.find('.tos input');
+  	countrySelect = container.find('.country');
+  	form = container.find('form');
+  	createButton = form.find('.btn');
+  	tosEl.change(function() {
+    	return createButton.toggleClass('disabled', !tosEl.is(':checked'));
+  	});
+  	form.submit(function(e) {
+    	if (!tosEl.is(':checked')) {
+      	e.preventDefault();
+      	return false;
+    }
+    	return createButton.addClass('disabled').val('...');
+  	});
+  	return countrySelect.change(function() {
+    	var termsUrl;
+    	termsUrl = "https://stripe.com/" + (countrySelect.val().toLowerCase()) + "/terms";
+    	return tosEl.siblings('a').attr({
+      		href: termsUrl
+    	});
+  	});
+};
 
+setupFieldsNeeded = function() {
+  	var container, form;
+  	container = $('.needed');
+  	if (container.length === 0) {
+    	return;
+  	}
+  	form = container.find('form');
+  	return form.submit(function(e) {
+    	var baContainer, button, tokenField;
+    	button = form.find('.buttons .btn');
+    	button.addClass('disabled').val('Saving...');
+    	if ((baContainer = form.find('#bank-account')).length > 0) {
+      		Stripe.setPublishableKey(baContainer.data('publishable'));
+      		tokenField = form.find('#bank_account_token');
+	      	if (tokenField.is(':empty')) {
+	        	e.preventDefault();
+	        	Stripe.bankAccount.createToken(form, function(_, resp) {
+	          	if (resp.error) {
+	            	button.removeClass('disabled').val('Save Info');
+	            	return alert(resp.error.message);
+	          	} else {
+	            	tokenField.val(resp.id);
+	            	return form.get(0).submit();
+	          	}
+	        });
+        	return false;
+      		}
+    	}
+  	});
+};
